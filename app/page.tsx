4d15ca635544dda7cef40492ac13f481f527fc26
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-03-07 18:23:09 +0700

    init project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+
+
+  return (
+    <main className="flex flex-col items-center px-4">
+      
+    </main>
+  );
+}

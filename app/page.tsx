20c356d930734a38691c2e20fb7f51bf469fe73a
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-03-09 03:40:39 +0700

    finished Vercel deploy. Cause: cannot use `react-useanimations` in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Intro from "@/components/intro"; // Err
-import Prelude from "@/components/prelude"; // YEAH
-import History from "@/components/history"; // YEAH
-import MiddleStory from "@/components/middle-story"; // YEAH
-import FutureStory from "@/components/future"; // YEAH
-import PresentStory from "@/components/present"; // YEAH
+import Intro from "@/components/intro";
+import Prelude from "@/components/prelude";
+import History from "@/components/history";
+import MiddleStory from "@/components/middle-story";
+import FutureStory from "@/components/future";
+import PresentStory from "@/components/present";
 
 
 export default function Home() {

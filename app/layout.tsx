9dbed93eae9923078daf2107163f3972168fa8f2
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-03-09 03:18:35 +0700

    fix: update import path for libreFranklin font in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { libreFranklin } from "./ui/fonts";
+import { libreFranklin } from "@/app/ui/fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
